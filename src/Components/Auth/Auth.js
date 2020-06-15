334b2c562dded4a5691d64125416c388d6d9e62a
TKSomar <tksomar@gmail.com>
2020-06-15 13:37:48 -0500

    Fixed Auth.js and updated Post.js to complete

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,76 +1,64 @@
-import React, {Component} from 'react';
-import axios from 'axios';
-import logo from './../../logo.png';
-import {loginUser} from '../../redux/reducer';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
+import { updateUser } from './../../ducks/reducer';
+
+import './Auth.css';
+import logo from './../../assets/logo.png';
 
 class Auth extends Component {
-    constructor(){
-        super();
-
-        this.state = {
-            username: '',
-            password: ''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
     }
-
-    changeHandler = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+    this.login = this.login.bind(this);
+    this.register = this.register.bind(this);
+  }
+  handleChange(prop, val) {
+    if (val.length < 12) {
+      this.setState({
+        [prop]: val
+      })
     }
-
-    login = (e) => {
-        const {username, password} = this.state;
-        axios.post('/api/auth/login', {username, password})
-        .then( res => {
-            this.props.loginUser(res.data)
-            this.props.history.push('/dashboard')
-        })
-        .catch(err => {
-            console.log(err)
-            alert('Could not log in')
-        })
-    }
-
-    register = (e) => {
-        const {username, password} = this.state;
-        axios.post('/api/auth/register', {username, password})
-        .then( res => {
-            this.props.loginUser(res.data)
-            this.props.history.push('/dashboard')
-        })
-        .catch(err => {
-            console.log(err.response.data)
-            alert(err.response.data)
-        })
-    }
-
-    render() {
-        const {username, password} = this.state
-        return (
-            <div className="Auth">
-                <div className="auth-container">
-                    <img src={logo} alt="logo"/>
-                    <h1 className="auth_title">Helo</h1>
-                        <div className="auth_input_box">
-                            <p className="auth_input_title">Username:</p>
-                            <input type="text" name="username" id="username" value={username} onChange={e => this.changeHandler(e)}></input>
-                        </div>
-                        <div className="auth_input_box">
-                            <p className="auth_input_title">Password:</p>
-                            <input type="password" name="password" id="password" value={password} onChange={e => this.changeHandler(e)}></input>
-                        </div>
-                        <div className="auth_button_container">
-                            <button className="dark_button" onClick={e => this.login(e)}>Login</button>
-                            <button className="dark_button" onClick={e => this.register(e)}>Register</button>
-                        </div>
-                </div>
-            </div>
-        )
-    }
+  }
+  login() {
+    axios.post('/api/auth/login', this.state)
+      .then(res => {
+        this.props.updateUser(res.data);
+        this.props.history.push('/dashboard');
+      })
+  }
+  register() {
+    axios.post('/api/auth/register', this.state)
+      .then(res => {
+        this.props.updateUser(res.data);
+        this.props.history.push('/dashboard');
+      })
+  }
+  render() {
+    return (
+      <div className='Auth'>
+        <div className='auth_container'>
+          <img src={logo} alt='logo' />
+          <h1 className='auth_title'>Helo</h1>
+          <div className='auth_input_box'>
+            <p>Username:</p>
+            <input value={this.state.username} onChange={e => this.handleChange('username', e.target.value)} />
+          </div>
+          <div className='auth_input_box'>
+            <p>Password:</p>
+            <input value={this.state.password} type='password' onChange={e => this.handleChange('password', e.target.value)} />
+          </div>
+          <div className='auth_button_container'>
+            <button className='dark_button' onClick={this.login}> Login </button>
+            <button className='dark_button' onClick={this.register}> Register </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = reduxState => reduxState
-
-export default connect(mapStateToProps, {loginUser})(Auth)
+export default connect(null, { updateUser })(Auth);
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,27 +1,61 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import './Post.css';
+import noImage from './../../no_image.jpg';
 
-export default class Post extends Component {
-    constructor(){
-        super();
-
-        this.state = {
-            title: '',
-            img: '',
-            content: '',
-            author: '',
-            authorPicture: ''
-        }
+class Post extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: '',
+      author_pic: '',
+      title: '',
+      img: '',
+      content: '',
+      loading: true
     }
-
-    getPostInfo(){
-        
-    }
-    
-    render() {
-        return (
-            <div>
-                <p>This is the Post Component.</p>
+  }
+  componentDidMount() {
+    axios.get(`/api/post/${this.props.match.params.id}`)
+      .then(res => {
+        setTimeout(_ => this.setState({ ...res.data, loading: false }), 500)
+      })
+  }
+  render() {
+    let imgSrc = this.state.img ? this.state.img : noImage;
+    return (
+      <div className='Post content_box'>
+        {!this.state.loading && this.state.title
+          ?
+          <div>
+            <div className='post_header'>
+              <h2 className='title'>{this.state.title}</h2>
+              <div className='author_box'>
+                <p>by {this.state.author}</p>
+                <img src={this.state.author_pic} alt='author' />
+              </div>
             </div>
-        )
-    }
+            <div className='post_content_box'>
+              <div className='post_img' style={{ backgroundImage: `url('${imgSrc}') ` }} alt='post' ></div>
+              <p>{this.state.content}</p>
+            </div>
+          </div>
+          :
+          !this.state.loading
+            ?
+            <div className='oops_box'>
+              <h2 className='title'>Oops!</h2>
+              <p>Looks like this post doesn't exist anymore</p>
+            </div>
+            :
+            <div className='load_box'>
+              <div className='load_background'></div>
+              <div className='load'></div>
+            </div>
+        }
+      </div>
+    )
+  }
 }
+
+export default Post;
